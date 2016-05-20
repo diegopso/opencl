21e34e8eed0e60cd918a7b05b545d951a290038e
Diego Oliveira <diegopso2@gmail.com>
2016-05-20 12:24:09 -0300

    integrando sem reducao

--- integrate-python/integrate.cl
+++ integrate-python/integrate.cl
@@ -1,30 +1,28 @@
-__kernel void integrate(__global float *a, unsigned int n)
-{
-  	int i = get_global_id(0), range;
-	float x = 1 / n * (i + 0.5);
-	a[i] = 4.0/(1.0 + x*x);
-
-	barrier();
-
-	range = ceil(n/10);
-	while(range > 10) {
-		reduce(i, range, n, a);
-		barrier();
-		n = range;
-		range = ceil(n/10);
-	}
-}
-
-void reduce(const int i, const int range, const unsigned int n, __global float *a)
+float reduce(const int i, int range, const unsigned int n, __global float *a)
 {
 	if(i < range) {
-		int j, limit = (i + 1) * range;
+		range = n / range;
+
+		int j;
+		int limit = (i + 1) * range;
 		float tmp = 0.0f;
 
+		//printf("%d, %d\n", limit, n);
+
 		for(j = i * range; j < limit && j < n; j++) {
-			tmp += a[j];			
+			tmp += a[j];
 		}
 
-		a[i] = tmp;
+		return tmp;
 	}
+
+	return 0.0f;
+}
+
+__kernel void integrate(__global float *a, unsigned int n)
+{
+  	int i = get_global_id(0);
+	float range, tmp, x = 1.0f / ((float) n) * (i + 0.5f);
+
+	a[i] = 4.0f / (1.0f + x*x);
 }
--- integrate-python/main.py
+++ integrate-python/main.py
@@ -2,15 +2,13 @@ import numpy
 import pyopencl as cl
 
 import os
-#os.environ['PYOPENCL_COMPILER_OUTPUT'] = '1'
+os.environ['PYOPENCL_COMPILER_OUTPUT'] = '1'
 os.environ['PYOPENCL_CTX'] = '0:1'
 
-N = 5
+N = 1000
 
 # create host arrays
 h_a = numpy.random.rand(N).astype(numpy.float32)
-h_b = numpy.random.rand(N).astype(numpy.float32)
-h_c = numpy.empty_like(h_a)
 
 # create context, queue and program
 context = cl.create_some_context()
@@ -20,19 +18,21 @@ program = cl.Program(context, kernelsource).build()
 
 # create device buffers
 mf = cl.mem_flags
-d_a = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=h_a)
-d_b = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=h_b)
-d_c = cl.Buffer(context, mf.WRITE_ONLY, h_c.nbytes)
+d_a = cl.Buffer(context, mf.WRITE_ONLY, h_a.nbytes)
 
 # run kernel
 integrate = program.integrate
-integrate.set_scalar_arg_dtypes([None, None, None, numpy.uint32])
-#vadd(queue, (5,), None, d_a, d_b, d_c, N)
-integrate(queue, h_a.shape, None, d_a, d_b, d_c, N)
+integrate.set_scalar_arg_dtypes([None, numpy.uint32])
+integrate(queue, h_a.shape, None, d_a, N)
 
 # return results
-cl.enqueue_copy(queue, h_c, d_c)
+cl.enqueue_copy(queue, h_a, d_a)
 
-print(h_a)
-print(h_b)
-print(h_c)
+sum = 0.0
+for i in range(N):
+	sum += h_a[i]
+
+sum /= N
+
+#print(h_a)
+print(sum)
