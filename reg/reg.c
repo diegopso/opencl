6350ac45ed8f618fc1dabd76061e3f94cd2a9d9b
mo802e08 <mo802e08@mexico.redes.lab.ic.unicamp.br>
2016-06-17 16:52:43 -0300

    for no codigo original

--- reg/reg.c
+++ reg/reg.c
@@ -7,7 +7,7 @@
 #include <semblance.h>
 #include <su.h>
 #include <errno.h>
-
+#include <sys/time.h>
 /*
  * compute_max finds the best parameters 'Aopt', 'Bopt', 'Copt', 'Dopt' and 'Eopt' 
  * that fit a curve to the data in 'ap' from a reference point (m0, h0, t0). Also 
@@ -19,146 +19,157 @@
  * the search space is specified through 'np'
  */
 void compute_max(aperture_t *ap, float m0, float h0, float t0,
-    const float p0[5], const float p1[5], const int np[5], float *Aopt,
-    float *Bopt, float *Copt, float *Dopt, float *Eopt, float *sem,
-    float *stack)
+		const float p0[5], const float p1[5], const int np[5], float *Aopt,
+		float *Bopt, float *Copt, float *Dopt, float *Eopt, float *sem,
+		float *stack)
 {
-    /* The parallel version of the code will compute the best parameters for 
-     * each value of the parameter 'A', so we need to store np[0] different 
-     * values of each parameter, stack and semblance */
-    float _Aopt[np[0]], _Bopt[np[0]], _Copt[np[0]], 
-          _Dopt[np[0]], _Eopt[np[0]];
-    float smax[np[0]];
-    float _stack[np[0]];
+	/* The parallel version of the code will compute the best parameters for 
+	 * each value of the parameter 'A', so we need to store np[0] different 
+	 * values of each parameter, stack and semblance */
+	float _Aopt[np[0]], _Bopt[np[0]], _Copt[np[0]], 
+	_Dopt[np[0]], _Eopt[np[0]];
+	float smax[np[0]];
+	float _stack[np[0]];
 
-    /* Split the outermost loop between threads. Each thread will
-     * compute the best fit for a given parameter 'A' value */
-    #pragma omp parallel for schedule(dynamic)
-    for (int ia = 0; ia < np[0]; ia++) {
+	/* Split the outermost loop between threads. Each thread will
+	 * compute the best fit for a given parameter 'A' value */
+#pragma omp parallel for schedule(dynamic)
+	for (int ia = 0; ia < np[0]; ia++) {
 
-        smax[ia] = -1;
-        float a = p0[0] + ((float)ia / (float)np[0]) * (p1[0]-p0[0]);
-        for (int ib = 0; ib < np[1]; ib++) {
+		smax[ia] = -1;
+		float a = p0[0] + ((float)ia / (float)np[0]) * (p1[0]-p0[0]);
+		for (int ib = 0; ib < np[1]; ib++) {
 
-            float b = p0[1] + ((float)ib / (float)np[1])*(p1[1]-p0[1]);
-                  b = p0[1] + ((float)ib / (float)np[1])*(p1[1]-p0[1]);
-            for (int ic = 0; ic < np[2]; ic++) {
+			float b = p0[1] + ((float)ib / (float)np[1])*(p1[1]-p0[1]);
+			b = p0[1] + ((float)ib / (float)np[1])*(p1[1]-p0[1]);
+			for (int ic = 0; ic < np[2]; ic++) {
 
-                float c = p0[2] + ((float)ic / (float)np[2])*(p1[2]-p0[2]);
-                for (int id = 0; id < np[3]; id++) {
+				float c = p0[2] + ((float)ic / (float)np[2])*(p1[2]-p0[2]);
+				for (int id = 0; id < np[3]; id++) {
 
-                    float d = p0[3] + ((float)id / (float)np[3])*(p1[3]-p0[3]);
-                    for (int ie = 0; ie < np[4]; ie++) {
+					float d = p0[3] + ((float)id / (float)np[3])*(p1[3]-p0[3]);
+					for (int ie = 0; ie < np[4]; ie++) {
 
-                        float e = p0[4] + ((float)ie / (float)np[4])*(p1[4]-p0[4]);
-                        float st;
-                        /* Check the fit of the parameters to the data and update the 
-                         * maximum for that point if necessary */
-                        float s = semblance_2d(ap, a, b, c, d, e, t0, m0, h0, &st);
-                        // printf("%d, %d, %d, %d, %d => %f\n", ia, ib, ic, id, ie, s);
-                        if (s > smax[ia]) {
-                            smax[ia] = s;
-                            _stack[ia] = st;
-                            _Aopt[ia] = a;
-                            _Bopt[ia] = b;
-                            _Copt[ia] = c;
-                            _Dopt[ia] = d;
-                            _Eopt[ia] = e;
-                        }
-                    }
-                }
-            }
-            /* Uncomment this to roughly check the progress */
-            /* fprintf(stderr, "."); */
-        }
-    }
+						float e = p0[4] + ((float)ie / (float)np[4])*(p1[4]-p0[4]);
+						float st;
+						/* Check the fit of the parameters to the data and update the 
+						 * maximum for that point if necessary */
+						 float s = semblance_2d(ap, a, b, c, d, e, t0, m0, h0, &st);
+						 // printf("%d, %d, %d, %d, %d => %f\n", ia, ib, ic, id, ie, s);
+						 if (s > smax[ia]) {
+							 smax[ia] = s;
+							 _stack[ia] = st;
+							 _Aopt[ia] = a;
+							 _Bopt[ia] = b;
+							 _Copt[ia] = c;
+							 _Dopt[ia] = d;
+							 _Eopt[ia] = e;
+						 }
+					}
+				}
+			}
+			/* Uncomment this to roughly check the progress */
+			/* fprintf(stderr, "."); */
+		}
+	}
 
-    /* Now find the best fit between different 'A' values */
-    float ssmax = -1.0;
-    *stack = 0;
-    for (int ia = 0; ia < np[0]; ia++) {
-        if (smax[ia] > ssmax) {
-            *Aopt = _Aopt[ia];
-            *Bopt = _Bopt[ia];
-            *Copt = _Copt[ia];
-            *Dopt = _Dopt[ia];
-            *Eopt = _Eopt[ia];
-            *stack = _stack[ia];
-            *sem = smax[ia];
-            ssmax = smax[ia];
-        }
-    }
+	/* Now find the best fit between different 'A' values */
+	float ssmax = -1.0;
+	*stack = 0;
+	for (int ia = 0; ia < np[0]; ia++) {
+		if (smax[ia] > ssmax) {
+			*Aopt = _Aopt[ia];
+			*Bopt = _Bopt[ia];
+			*Copt = _Copt[ia];
+			*Dopt = _Dopt[ia];
+			*Eopt = _Eopt[ia];
+			*stack = _stack[ia];
+			*sem = smax[ia];
+			ssmax = smax[ia];
+		}
+	}
 }
 
 int main(int argc, char *argv[])
 {
-    int i;
+	int i;
 
-    if (argc != 21) {
-        fprintf(stderr, "Usage: %s M0 H0 T0 TAU A0 A1 NA B0 B1 NB "
-            "C0 C1 NC D0 D1 ND E0 E1 NE INPUT\n", argv[0]);
-        exit(1);
-    }
+	if (argc != 21) {
+		fprintf(stderr, "Usage: %s M0 H0 T0 TAU A0 A1 NA B0 B1 NB "
+				"C0 C1 NC D0 D1 ND E0 E1 NE INPUT\n", argv[0]);
+		exit(1);
+	}
 
-    float m0 = atof(argv[1]);
-    float h0 = atof(argv[2]);
-    float t0 = atof(argv[3]);
-    float tau = strtof(argv[4], NULL);
+	float m0 = atof(argv[1]);
+	float h0 = atof(argv[2]);
+	float t0 = atof(argv[3]);
+	float tau = strtof(argv[4], NULL);
 
-    /* A, B, C, D, E */
-    float p0[5], p1[5];
-    int np[5];
+	/* A, B, C, D, E */
+	float p0[5], p1[5];
+	int np[5];
 
-    /* p0 is where the search starts, p1 is where the search ends and np is the 
-     * number of points in between p0 and p1 to do the search */   
-    for (i = 0; i < 5; i++) {
-        p0[i] = atof(argv[5 + 3*i]);
-        p1[i] = atof(argv[5 + 3*i + 1]);
-        np[i] = atoi(argv[5 + 3*i + 2]);
-    }
+	/* p0 is where the search starts, p1 is where the search ends and np is the 
+	 * number of points in between p0 and p1 to do the search */   
+	for (i = 0; i < 5; i++) {
+		p0[i] = atof(argv[5 + 3*i]);
+		p1[i] = atof(argv[5 + 3*i + 1]);
+		np[i] = atoi(argv[5 + 3*i + 2]);
+	}
 
-    /* Load the traces from the file */
+	/* Load the traces from the file */
 
-    char *path = argv[20];
-    FILE *fp = fopen(path, "r");
+	char *path = argv[20];
+	FILE *fp = fopen(path, "r");
 
-    if (!fp) {
-        fprintf(stderr, "Failed to open prestack file '%s'!\n", path);
-        return 1;
-    }
+	if (!fp) {
+		fprintf(stderr, "Failed to open prestack file '%s'!\n", path);
+		return 1;
+	}
 
-    su_trace_t tr;
-    vector_t(su_trace_t) traces;
-    vector_init(traces);
+	su_trace_t tr;
+	vector_t(su_trace_t) traces;
+	vector_init(traces);
 
-    while (su_fgettr(fp, &tr)) {
-        vector_push(traces, tr);
-    }
+	while (su_fgettr(fp, &tr)) {
+		vector_push(traces, tr);
+	}
 
-    /* Construct the aperture structure from the traces, which is a vector
-     * containing pointers to traces */
+	/* Construct the aperture structure from the traces, which is a vector
+	 * containing pointers to traces */
 
-    aperture_t ap;
-    ap.ap_m = 0;
-    ap.ap_h = 0;
-    ap.ap_t = tau;
-    vector_init(ap.traces);
-    for (int i = 0; i < traces.len; i++)
-        vector_push(ap.traces, &vector_get(traces, i));
+	aperture_t ap;
+	ap.ap_m = 0;
+	ap.ap_h = 0;
+	ap.ap_t = tau;
+	vector_init(ap.traces);
+	for (int i = 0; i < traces.len; i++)
+		vector_push(ap.traces, &vector_get(traces, i));
 
-    /* Find the best parameter combination */
+	/* Find the best parameter combination */
 
-    float a, b, c, d, e, sem, stack;
-    compute_max(&ap, m0, h0, t0, p0, p1, np, &a, &b, &c, &d, &e, &sem, &stack);
+	float a, b, c, d, e, sem, stack;
 
-    printf("A=%g\n", a);
-    printf("B=%g\n", b);
-    printf("C=%g\n", c);
-    printf("D=%g\n", d);
-    printf("E=%g\n", e);
-    printf("Stack=%g\n", stack);
-    printf("Semblance=%g\n", sem);
-    printf("\n");
+	struct timeval  tv1, tv2;
 
-    return 0;
+	for(int i = 0; i < 10; i++) {
+		gettimeofday(&tv1, NULL);
+		compute_max(&ap, m0, h0, t0, p0, p1, np, &a, &b, &c, &d, &e, &sem, &stack);
+		gettimeofday(&tv2, NULL);
+
+		printf ("%0.3f\n",
+				((double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
+						(double) (tv2.tv_sec - tv1.tv_sec)) * 1000 );
+	}
+
+	printf("A=%g\n", a);
+	printf("B=%g\n", b);
+	printf("C=%g\n", c);
+	printf("D=%g\n", d);
+	printf("E=%g\n", e);
+	printf("Stack=%g\n", stack);
+	printf("Semblance=%g\n", sem);
+	printf("\n");
+
+	return 0;
 }
